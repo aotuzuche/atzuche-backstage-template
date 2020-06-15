443a204727ea60aa26eb6a424b156b8fa0ba82a6
shaodahong <darryshaw@gmail.com>
2020-06-15 18:19:08 +0800

    improve: 去掉 projectType 模版变量

--- appConfig.js
+++ appConfig.js
@@ -1,14 +1,14 @@
 module.exports = {
   // 一般情况下，它的值是 /${prodPathPrefix}/${prodPath}
-  basename: process.env.NODE_ENV === 'production' ? '/<%= projectType %>/<%= prodPath %>' : '',
+  basename: process.env.NODE_ENV === 'production' ? '/system/<%= prodPath %>' : '',
 
   // 打包出口目录的前缀，注意：不需要以/开头
-  prodPathPrefix: '<%= projectType %>',
+  prodPathPrefix: 'system',
 
   // 打包的出口目录(默认dist目录)
   prodPath: '<%= prodPath %>',
 
-  // syscode 就是打包出口慕丽的名称
+  // syscode 就是打包出口目录的名称
   syscode: '<%= prodPath %>',
 
   // 本地测试端口
