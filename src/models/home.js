7d6ec0babb8c1e51c35d0a3250083189d05e1ca5
shaodahong <darryshaw@gmail.com>
2020-06-20 22:22:46 +0800

    refactor: ts

--- src/models/home.js
+++
@@ -1,9 +0,0 @@
-export default {
-  namespace: 'home',
-  state: [],
-  reducers: {
-    set(state, { payload }) {
-      return { ...state, ...payload }
-    },
-  },
-}
--- src/utils/arraryHelp.js
+++
@@ -1,5 +0,0 @@
-const isFalse = icon => {
-  return ['false', false, 0, '0'].includes(icon)
-}
-
-export { isFalse }
